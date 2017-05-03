5de1f7e51c7ccbc16834768622854ae7bec01f41
liuzh811 <liuzh811@gmail.com>
2017-05-03 15:03:37 -0600

    Create learning.materials.r

---
+++ learning.materials.r
@@ -0,0 +1,6 @@
+# some learning material from web
+
+1. bootstrapping or monto carlo:: Regression Model Estimators
+http://sites.stat.psu.edu/~dhunter/astrostatistics/PennState2005/Bootstrap.html
+http://davegiles.blogspot.com/2016/12/monte-carlo-simulation-basics-iii.html
+
