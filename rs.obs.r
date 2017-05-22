0f1fedef75bd60cf5c77335058336c5c178c6ae4
liuzh811 <liuzh811@gmail.com>
2017-05-22 16:22:49 -0600

    Update rs.obs.r

--- rs.obs.r
+++ rs.obs.r
@@ -1,1 +1,7 @@
 ## 5/22/2017
+## using remote sensing and co2 inversion method to calculate the relationship between GPP/ER with precipitation and temperature
+
+## using the median/mean NEE estimate from four co2 inversion methods
+
+
+
